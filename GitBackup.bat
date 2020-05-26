bb107cb7d15cb97983e4910dadd40602970f2238
dilaccode <congtk1992@gmail.com>
2020-05-26 18:19:53 +0700

    work backup: code, db

---
+++ GitBackup.bat
@@ -0,0 +1,5 @@
+@echo off
+C:\xampp\mysql\bin\mysqldump.exe --user=root --password= --host=localhost --port=3306 --result-file="C:\xampp\htdocs\database\word_database.sql" --databases "word"
+git add -A
+git commit -m "work backup: code, db"
+git push
