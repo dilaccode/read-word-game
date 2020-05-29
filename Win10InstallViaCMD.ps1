2ecb9ae82f9e21bc411731b585a9c252d167e2e9
dilaccode <65000382+dilaccode@users.noreply.github.com>
2020-05-29 15:18:14 +0700

    Update Win10InstallViaCMD.ps1

--- Win10InstallViaCMD.ps1
+++ Win10InstallViaCMD.ps1
@@ -6,9 +6,10 @@ choco install -y git.install
 
 :: Install Xampp
 choco install -y bitnami-xampp
-	<# start apache, mysql #>
-	Start-Process C:\xampp\mysql\bin\mysqld.exe
-	Start-Process C:\xampp\apache\bin\httpd.exe
+	:: start apache, mysql
+	start C:\xampp\mysql\bin\mysqld.exe
+	:: start xampp for start apache
+	start C:\xampp\xampp-control.exe
   
 :: Install Netbeans (will include JDK version X)
 choco install -y netbeans-php
