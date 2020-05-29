0c81ae43b5d04ac37175c5dc1aa1b8e51814f890
dilaccode <congtk1992@gmail.com>
2020-05-29 15:57:26 +0700

    oh

--- Win10AutoInstall/Win10InstallSoftwareStart.ps1
+++ Win10AutoInstall/Win10InstallSoftwareStart.ps1
@@ -5,6 +5,6 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 <# Download download .BAT file for install all software and excute #>
-Invoke-WebRequest https://raw.githubusercontent.com/dilaccode/read-word-game/master/Win10InstallSoftwareMain.bat -OutFile C:\Win10InstallSoftwareMain.bat
+Invoke-WebRequest https://raw.githubusercontent.com/dilaccode/read-word-game/master/Win10AutoInstall/Win10InstallSoftwareMain.bat -OutFile C:\Win10InstallSoftwareMain.bat
 <# Execute #>
 Start-Process C:\Win10InstallSoftwareMain.bat
