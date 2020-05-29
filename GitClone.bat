f55cab3a7068609fd8018486e3a1ca52fe16dafc
dilaccode <65000382+dilaccode@users.noreply.github.com>
2020-05-29 14:43:05 +0700

    Add files via upload

---
+++ GitClone.bat
@@ -0,0 +1,18 @@
+@echo off
+:: config git
+git config --global user.email "congtk1992@gmail.com"
+git config --global user.name "dilaccode"
+
+:: clone source
+cd c:\
+RMDIR "C:\xampp\htdocs" /S /Q
+MKDIR C:\xampp\htdocs
+cd C:\xampp\htdocs
+git clone https://github.com/dilaccode/read-word-game.git .
+
+:: execute databse
+C:\xampp\mysql\bin\mysql.exe --user=root --password= --host=localhost --port=3306 < "C:\xampp\htdocs\database\word_database.sql"
+
+:: open test site
+explorer "https://localhost"
+
