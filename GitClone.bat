8d8343b5646ca47cfb910d7a53b579e5c96a083d
dilaccode <congtk1992@gmail.com>
2020-05-30 17:21:36 +0700

    add config netbean, udpate GitClone

--- GitClone.bat
+++ GitClone.bat
@@ -13,7 +13,13 @@ git clone https://github.com/dilaccode/read-word-game.git .
 :: execute databse
 C:\xampp\mysql\bin\mysql.exe --user=root --password= --host=localhost --port=3306 < "C:\xampp\htdocs\database\word_database.sql"
 
+:: copy apache config
+xcopy /R /Y "C:\xampp\htdocs\server_setup\XAMPP_httpd.conf" "C:\xampp\apache\conf\httpd.conf"
+C:\Users\Administrator\Desktop\httpd.conf
+:: start mysql
+start C:\xampp\mysql\bin\mysqld.exe
+
 :: open test site
-explorer "https://127.0.0.1:80"
-explorer "https://127.0.0.1:81"
+explorer "https://localhost"
+explorer "https://localhost:81"
 
