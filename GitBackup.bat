87adde77f3bc9334ed8994a60491a8318965ddc7
dilaccode <congtk1992@gmail.com>
2020-06-05 21:17:48 +0700

    work backup: code, db. add some sound

--- GitBackup.bat
+++ GitBackup.bat
@@ -1,6 +1,7 @@
 @echo off
 cd C:\xampp\htdocs\
 C:\xampp\mysql\bin\mysqldump.exe --user=root --password= --host=localhost --port=3306 --result-file="C:\xampp\htdocs\database\word_database.sql" --databases "word"
+set /p Message="more message (optional): "
 git add -A
-git commit -m "work backup: code, db"
+git commit -m "work backup: code, db. %Message%"
 git push
