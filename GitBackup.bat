45dc5fb72d149412752bcf0348939b07c1b16b82
dilaccode <congtk1992@gmail.com>
2020-06-05 21:20:37 +0700

    work backup: code, db. test backup

--- GitBackup.bat
+++ GitBackup.bat
@@ -1,6 +1,8 @@
 @echo off
 cd C:\xampp\htdocs\
 C:\xampp\mysql\bin\mysqldump.exe --user=root --password= --host=localhost --port=3306 --result-file="C:\xampp\htdocs\database\word_database.sql" --databases "word"
+:: default
+set Message = "."
 set /p Message="more message (optional): "
 git add -A
 git commit -m "work backup: code, db. %Message%"
