aaf756b0b7350a149bbc4f4aaba3d78449e6a5e5
dilaccode <congtk1992@gmail.com>
2020-07-03 03:33:26 +0700

    update background images

--- SERVER/app/Views/Stats/Header.php
+++ SERVER/app/Views/Stats/Header.php
@@ -3,8 +3,8 @@
 
 <!-- title -->
 <div class='w3-xxlarge w3-indigo upper w3-center'>
-    Stats
+    STATS
 </div>
 <button class="w3-button w3-blue w3-round-large w3-display-topleft"
         style="margin: 4px"
-        onclick="window.history.back();"><= Back</button>
+        onclick="window.history.back();"><= BACK</button>
